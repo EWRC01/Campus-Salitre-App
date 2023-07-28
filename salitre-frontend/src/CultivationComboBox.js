12ff86ae78a64d3c3aea9166e962fe9cd4560672
Adonay Aragón <119976181+Adonay117@users.noreply.github.com>
2023-07-27 21:35:56 -0600

    Update CultivationComboBox.js

--- salitre-frontend/src/CultivationComboBox.js
+++ salitre-frontend/src/CultivationComboBox.js
@@ -1,31 +1,53 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
+import { Select, MenuItem } from '@mui/material';
 
-function CultivationComboBox({name, value, onChange}) {
-    const [cultivationTypes, setCultivationTypes] = useState([]);
+function CultivationComboBox({ name, value, onChange }) {
+  const [cultivationTypes, setCultivationTypes] = useState([]);
 
-    useEffect(() => {
-        axios.get('http://localhost:5000/api/types-cultivation')
-            .then((response) => {
-                setCultivationTypes(response.data);
-            })
-            .catch((error) => {
-                console.error('Error during fetching cultivation types: ', error);
-            });
+  const comboBoxStyle = {
+    padding: '6px 12px', // Ajusta el padding para reducir el tamaño del ComboBox
+    borderRadius: '5px',
+    backgroundColor: '#f5f5f5',
+    width: '100%',
+    outline: 'none',
+    userSelect: 'none',
+    fontSize: '14px', // Ajusta el tamaño de fuente para reducir el tamaño del texto
+  };
 
-    }, [])
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/types-cultivation')
+      .then((response) => {
+        setCultivationTypes(response.data);
+      })
+      .catch((error) => {
+        console.error('Error during fetching cultivation types: ', error);
+      });
 
-    return (
-        <select name={name} value={value} onChange={onChange}>
-            {/* Add an initial Blank Option */}
-            <option value=""> Selecciona una opcion por favor</option>
-            {cultivationTypes.map((type) => (
-                <option key={type.ID_Type_Cultivation} value={type.ID_Type_Cultivation}>
-                    {type.Name_Type_Cultivation}
-                </option>
-            ))}
-        </select>
-    );
+  }, []);
+
+  return (
+    <Select
+      name={name}
+      value={value}
+      onChange={onChange}
+      sx={comboBoxStyle}
+      variant="outlined"
+      fullWidth
+    >
+      {/* Add an initial Blank Option */}
+      <MenuItem value="">
+        Selecciona una opción por favor
+      </MenuItem>
+      {cultivationTypes.map((type) => (
+        <MenuItem key={type.ID_Type_Cultivation} value={type.ID_Type_Cultivation}>
+          {type.Name_Type_Cultivation}
+        </MenuItem>
+      ))}
+    </Select>
+  );
 }
 
 export default CultivationComboBox;
+
+
