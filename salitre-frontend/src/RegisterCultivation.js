7cfbc2678dd3927b6aa4ab5b569af92f7ac7a214
Adonay Aragón <119976181+Adonay117@users.noreply.github.com>
2023-07-27 21:36:48 -0600

    Update RegisterCultivation.js

--- salitre-frontend/src/RegisterCultivation.js
+++ salitre-frontend/src/RegisterCultivation.js
@@ -1,65 +1,119 @@
-import React,  { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { Box, Typography, TextField, Button } from '@mui/material';
 import ErrorAlert from "./ErrorAlert";
 import SuccessAlert from "./SuccessAlert";
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import CultivationComboBox from "./CultivationComboBox";
 
-
 function RegisterCultivation() {
+  const [nameCultivation, setNameCultivation] = useState('');
+  const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const navigate = useNavigate();
 
-    const [nameCultivation, setNameCultivation] = useState('');
-    const [error, setError] = useState(null);
-    const [successMessage, setSuccessMessage] = useState(null);
-    const navigate = useNavigate();
+  const Redirect = () => {
+    navigate('/features-crop');
+  }
 
-    const Redirect = () => {
-        navigate('/features-crop');
-    }
+  const containerStyle = {
+    maxWidth: '400px',
+    margin: '0 auto',
+    padding: '20px',
+    borderRadius: '10px',
+    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+    backgroundColor: '#ffffff',
+    userSelect: 'none', // Evita que se pueda seleccionar el contenido
+  };
 
-    const handleCultivationRegister = (e) => {
-        e.preventDefault();
+  const headingStyle = {
+    fontSize: '24px',
+    color: '#333',
+    marginBottom: '20px',
+  };
 
-        //call the api
-        axios.post('http://localhost:5000/api/registertypecultivation', {
-            nameCultivation
-        })
-        .then((response) => {
-            console.log('Cultivo Registrado con exito');
-            setSuccessMessage('Cultivo Registrado con exito');
-            
-            
-        })
-        .catch((error) => {
-            alert('Error during register the crop')
-            setError('Erro al registrar el cultivo');
-        });
-    };
+  const inputStyle = {
+    marginBottom: '20px',
+  };
 
-    return (
-        <form onSubmit={handleCultivationRegister}>
-        {error && <ErrorAlert message={error} />} {/* Mostrar el alerta de error si hay un mensaje de error */}
-        {successMessage && <SuccessAlert message={successMessage} />} {/* Mostrar el alerta de éxito si hay un mensaje de éxito */}
-        <h2>Cultivos Existentes: </h2>
-        <CultivationComboBox/>
-        <h2>Agrega un nuevo cultivo</h2>
-        
-        <div>
-            <label htmlFor="cultivationName">Nombre de cultivo: </label>
-            <input
-            type="text"
-            id="cultivationName"
-            value={nameCultivation}
-            required
-            onChange={(e) => setNameCultivation(e.target.value)}/>
-        </div>
-        <button type="submit">Agregar el cultivo</button>
-        <button type="block" onClick={Redirect}>Agregar Requerimientos</button>
-       
-        </form>
+  const buttonStyle = {
+    fontSize: '18px',
+    padding: '10px 20px',
+    borderRadius: '5px',
+    backgroundColor: '#007BFF',
+    color: '#fff',
+    border: 'none',
+    cursor: 'pointer',
+    width: '100%',
+    outline: 'none',
+    userSelect: 'none',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: '10px', // Agrega un espacio entre los botones
+  };
 
-    );
+  
+  const buttonStyle2 = {
+    fontSize: '18px',
+    padding: '10px 20px',
+    borderRadius: '5px',
+    backgroundColor: '#567665',
+    color: '#fff',
+    border: 'none',
+    cursor: 'pointer',
+    width: '100%',
+    outline: 'none',
+    userSelect: 'none',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: '10px', // Agrega un espacio entre los botones
+  };
+
+ 
+  const handleCultivationRegister = (e) => {
+    e.preventDefault();
+
+    // Call the API to register the cultivation
+    axios.post('http://localhost:5000/api/registertypecultivation', {
+      nameCultivation
+    })
+    .then((response) => {
+      console.log('Cultivo Registrado con exito');
+      setSuccessMessage('Cultivo Registrado con exito');
+    })
+    .catch((error) => {
+      setError('Error al registrar el cultivo');
+    });
+  };
+
+  return (
+    <Box sx={containerStyle}>
+      {error && <ErrorAlert message={error} />}
+      {successMessage && <SuccessAlert message={successMessage} />}
+      <Typography variant="h2" sx={headingStyle}>Cultivos Existentes:</Typography>
+      <CultivationComboBox />
+      <Typography variant="h2" sx={headingStyle}>Agrega un nuevo cultivo</Typography>
+      <TextField
+        label="Nombre de cultivo"
+        variant="outlined"
+        value={nameCultivation}
+        required
+        fullWidth
+        onChange={(e) => setNameCultivation(e.target.value)}
+        sx={inputStyle}
+      />
+      <Button variant="contained" color="primary" type="submit" fullWidth sx={buttonStyle}>
+        <span>Agregar el cultivo</span>
+        <span>&#43;</span> {/* Icono de más para el botón */}
+      </Button>
+      <Button variant="contained"  fullWidth onClick={Redirect} sx={buttonStyle2}>
+        <span>Agregar Requerimientos</span>
+        <span>&#43;</span> {/* Icono de más para el botón */}
+      </Button>
+    </Box>
+  );
 }
-
 
 export default RegisterCultivation;
