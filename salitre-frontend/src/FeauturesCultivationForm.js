8eb92b1b46503611ffe7c2d144a0bd657d6430f1
Adonay Aragón <119976181+Adonay117@users.noreply.github.com>
2023-07-27 21:35:19 -0600

    Update FeauturesCultivationForm.js

--- salitre-frontend/src/FeauturesCultivationForm.js
+++ salitre-frontend/src/FeauturesCultivationForm.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 import CultivationComboBox from './CultivationComboBox';
 import ErrorAlert from './ErrorAlert';
 import SuccessAlert from './SuccessAlert';
-import {  useNavigate } from 'react-router-dom';
+import { Box, Typography, TextField, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 function FeaturesCultivationForm() {
   const [formData, setFormData] = useState({
@@ -18,7 +21,7 @@ function FeaturesCultivationForm() {
 
   const Redirect = () => {
     navigate('/register-crop');
-}
+  }
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -45,52 +48,75 @@ function FeaturesCultivationForm() {
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
+    <Box sx={{ maxWidth: '400px', margin: '0 auto', padding: '20px', borderRadius: '10px', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)', backgroundColor: '#ffffff' }}>
+      <Typography variant="h4" align="center" sx={{ marginBottom: '20px' }}>
+        Agregar Requerimientos de Cultivo
+      </Typography>
+      <form onSubmit={handleFormSubmit}>
         {error && <ErrorAlert message={error} />} {/* Mostrar el alerta de error si hay un mensaje de error */}
         {successMessage && <SuccessAlert message={successMessage} />} {/* Mostrar el alerta de éxito si hay un mensaje de éxito */}
-        <h2>Agregar Requerimientos de Cultivo</h2>
-        
-      <div>
-        <label htmlFor="ID_Type_Cultivation">Tipo de Cultivo:</label>
-        <CultivationComboBox
-          name="ID_Type_Cultivation"
-          value={formData.ID_Type_Cultivation}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="Required_Relative_Humidity">Humedad Relativa Requerida:</label>
-        <input
+        <div>
+          <label htmlFor="ID_Type_Cultivation">Tipo de Cultivo:</label>
+          <CultivationComboBox
+            name="ID_Type_Cultivation"
+            value={formData.ID_Type_Cultivation}
+            onChange={handleInputChange}
+          />
+        </div>
+        <TextField
+          label="Humedad Relativa Requerida"
+          variant="outlined"
           type="text"
           name="Required_Relative_Humidity"
           value={formData.Required_Relative_Humidity}
           onChange={handleInputChange}
           required
+          fullWidth
+          sx={{ marginBottom: '20px' }}
         />
-      </div>
-      <div>
-        <label htmlFor="Required_Temperature">Temperatura Requerida:</label>
-        <input
+        <TextField
+          label="Temperatura Requerida"
+          variant="outlined"
           type="text"
           name="Required_Temperature"
           value={formData.Required_Temperature}
           onChange={handleInputChange}
           required
+          fullWidth
+          sx={{ marginBottom: '20px' }}
         />
-      </div>
-      <div>
-        <label htmlFor="Required_Oxygen">Oxigeno Requerido:</label>
-        <input
+        <TextField
+          label="Oxigeno Requerido"
+          variant="outlined"
           type="text"
           name="Required_Oxygen"
           value={formData.Required_Oxygen}
           onChange={handleInputChange}
           required
+          fullWidth
+          sx={{ marginBottom: '20px' }}
         />
-      </div>
-      <button type="submit">Añadir</button>
-      <button onClick={Redirect}>Regresar a tipo de cultivo</button>
-    </form>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          fullWidth
+          startIcon={<AddCircleIcon />}
+        >
+          Añadir
+        </Button>
+        <Box sx={{ height: '10px' }} /> {/* Espacio entre los botones */}
+        <Button
+          variant="contained"
+          color="secondary"
+          fullWidth
+          onClick={Redirect}
+          startIcon={<ArrowBackIcon />}
+        >
+          Regresar a tipo de cultivo
+        </Button>
+      </form>
+    </Box>
   );
 }
 
