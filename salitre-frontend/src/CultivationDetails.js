84ad8482d24f22a190bbf530df6404ecc4ba10ab
Adonay Aragón <119976181+Adonay117@users.noreply.github.com>
2023-07-29 20:18:09 +0000

    Update CultivationDetails.js

--- salitre-frontend/src/CultivationDetails.js
+++ salitre-frontend/src/CultivationDetails.js
@@ -1,24 +1,41 @@
 import React, { useState } from 'react';
 import CultivationComboBox from './CultivationComboBox';
 import AllCrops from './AllCrops';
+import { Box, Typography } from '@mui/material';
 
-const CultivationDetails = ({cultivationData}) => {
+const CultivationDetails = ({ cultivationData }) => {
   const [selectedCultivation, setSelectedCultivation] = useState("");
 
   const handleCultivationChange = (event) => {
     setSelectedCultivation(event.target.value);
   };
 
+  const containerStyle = {
+    maxWidth: '400px',
+    margin: '20px auto',
+    padding: '20px',
+    borderRadius: '10px',
+    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+    backgroundColor: '#ffffff',
+    userSelect: 'none', // Evita que se pueda seleccionar el contenido
+  };
+
+  const headingStyle = {
+    fontSize: '24px',
+    color: '#333',
+    marginBottom: '20px',
+  };
+
   return (
-    <div>
-      <h1>Selecciona un cultivo para ver los detalles</h1>
+    <Box sx={containerStyle}>
+      <Typography variant="h1" sx={headingStyle}>Selecciona un cultivo para ver los detalles</Typography>
       <CultivationComboBox
         name="cultivation"
         value={selectedCultivation}
         onChange={handleCultivationChange}
       />
       <AllCrops selectedCultivation={selectedCultivation} />
-    </div>
+    </Box>
   );
 };
 
